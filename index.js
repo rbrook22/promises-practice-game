b9c1d999e9c5f4f0e254d8ac9be5be3c98c651dd
Richard Brooks <rickbrooks22@gmail.com>
2018-08-04 20:14:59 -0400

    added timeout function

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let counter = 0;
 document.querySelector('button').addEventListener('click', () => {
     ++counter;
 });
+
+setTimeout(() => {
+    if(counter > 5) {
+        alert('You won!');
+    }else {
+        alert('Sorry, you lost!');
+    }
+}, 2000)
