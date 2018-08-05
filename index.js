e8d1af5b7f6ee764f691540e46942fbefdd37228
Richard Brooks <rickbrooks22@gmail.com>
2018-08-04 20:26:05 -0400

    wrapped logic in startgame function

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-let counter = 0;
+function startGame() {
+    let counter = 0;
 
-document.querySelector('button').addEventListener('click', () => {
-    ++counter;
-});
+    document.querySelector('button').addEventListener('click', () => {
+        ++counter;
+    });
 
-setTimeout(() => {
-    if(counter > 5) {
-        alert(`You won! Your click count was ${counter} clicks!`);
-    }else {
-        alert('Sorry, you lost!');
-    }
-}, 2000)
+    setTimeout(() => {
+        if(counter > 5) {
+            alert(`You won! Your click count was ${counter} clicks!`);
+        }else {
+            alert('Sorry, you lost!');
+        }
+    }, 2000)
+}
+
+startGame();
